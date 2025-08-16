659d885889f40e8c2e8bd4bd07007c1879836ef8
07Rafael08 <faelfernando1408@gmail.com>
2025-08-16 17:18:55 -0300

    add files

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return(
+        <header>
+            <h1>Vigio</h1>
+            <nav>
+                <ul>
+                    
+                </ul>
+            </nav>
+        </header>
+    )
+}
