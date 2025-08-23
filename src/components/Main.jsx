5e5bb0301f02be432a86dade5d0c3bc9d69ed753
07Rafael08 <faelfernando1408@gmail.com>
2025-08-23 20:48:42 -0300

    Change version PT-BR

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ export default function Main() {
     return(
         <main className="p-main w-screen h-screen">
             <section className="bg-[#d8d8d8] w-full h-full rounded-2xl flex flex-col justify-center items-center">
-                <h1 className='text-[38px]'>hello, world.</h1>
+                <h1 className='capitalize text-[38px]'>hello, world.</h1>
                 <h2>Em desenvolvimento...</h2>
             </section>
         </main>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,15 +7,15 @@ export default function NavBar() {
             <a href='/'><img width={140} src={SinuaSVG} /></a>
             <nav className='flex flex-row gap-8'>
                 <ul className='flex flex-row items-center gap-3'>
-                    <Links name="Home" link="/" />
-                    <Links name="About" link="/about" />
-                    <Links name="Projects" link="/projects" />
-                    <Links name="Server" link="/" />
-                    <Links name="Contact" link="/contact" />
+                    <Links name="Início" link="/" />
+                    <Links name="Sobre nós" link="/about" />
+                    <Links name="Projetos" link="/projects" />
+                    <Links name="Servidor" link="/" />
+                    <Links name="Contato" link="/contact" />
                 </ul>
                 <ul className='flex flez-row gap-4'>
-                    <button className='p bg-black text-white rounded-[12px] cursor-pointer duration-[0.3s] hover:opacity-[0.85]'>Login</button>
-                    <button className='p rounded-[12px] border-[3px] border-black cursor-pointer duration-[0.3s] hover:opacity-[0.65]'>Sign Up</button>
+                    <button className='p bg-black text-white rounded-[12px] cursor-pointer duration-[0.3s] hover:opacity-[0.85]'>Entrar</button>
+                    <button className='p rounded-[12px] border-[3px] border-black cursor-pointer duration-[0.3s] hover:opacity-[0.65]'>Registrar</button>
                 </ul>
             </nav>
         </header>
